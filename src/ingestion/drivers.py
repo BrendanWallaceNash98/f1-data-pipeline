5c01e33ebd7ba0a1d646fa404a8df1cf2205894d
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2026-05-26 20:08:45 +1000

    added .gitignore

--- src/ingestion/drivers.py
+++ src/ingestion/drivers.py
@@ -1,5 +1,31 @@
 from src.ingestion.api_client import fetch_data
+from src.storage.parquet_writer import write_parquet_file
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+
+formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('drivers')
+log_name = 'src/logs/drivers/drivers.log'
+os.makedirs(os.path.dirname(log_name), exist_ok=True)
+handler = TimedRotatingFileHandler(log_name, when='D', interval=1)
+handler.suffix = "%Y-%m-%d"
+handler.formatter = formater
+logger.addHandler(handler)
 
 def process_drivers():
-    driver_data = fetch_data("drivers")
+    endpoint = f"drivers"
+    logger.info(f"Calling Drivers endpoint: {endpoint}")
+    driver_data = fetch_data(endpoint)
+    ## I am not raising any concern for it returning nothing
+    ## as there will be days between races where we will get nothing
+    logger.info(f"Number of Driver records fetched: {len(driver_data)}")
+    try:
+        write_parquet_file(driver_data, 'drivers.parquet', False)
+    except Exception as e:
+        logger.error(f"Error writing to parquet file: {e}")
+        raise e
+    logger.info(f"Driver data collection processed successfully")
 
+process_drivers()
--- src/storage/parquet_writer.py
+++ src/storage/parquet_writer.py
@@ -1,20 +1,42 @@
-import json
 import os
-
 import pandas as pd
 
-def write_parquet_file(data, file_name: str, incremetal_load: bool = False):
+def write_parquet_file(data, file_name: str, incremental_load: bool = False):
     file_name = os.path.join("data/raw/", file_name)
 
     fetched_df = pd.DataFrame(data)
     ## country code will be deprecated in 2026 so just removing now
-    fetched_df = fetched_df.drop(['country_code'], axis=1, errors='ignore')
-    fetched_df = fetched_df.drop_duplicates()
-    if incremetal_load == False and os.path.exists(file_name):
-        return fetched_df
+    fetched_df = process_fetched_data(fetched_df)
+
+    if not incremental_load:
+        fetched_df.to_parquet(file_name)
+        return
+    ## this is to assume it is its first run
+    if not os.path.exists(file_name):
+        fetched_df.to_parquet(file_name)
+        return
+
+    incremental_load_data(fetched_df, file_name)
+    return
 
 
 
+def process_fetched_data(df: pd.DataFrame) -> pd.DataFrame:
+    ## country code will be deprecated in 2026 so just removing now
+    df = df.drop(['country_code'], axis=1, errors='ignore')
+    ## some of the columns are returned as lists, I am just converting them to string
+    ## this allows for a simpler drop_duplicates() function to be used but also
+    ## will likely be easier for db ingestion
+    lst_cols = [col for col in df.columns if df[col].apply(lambda x: isinstance(x, list)).any()]
+    for col in lst_cols:
+        df[col] = df[col].astype(str)
 
+    df = df.drop_duplicates()
+    return df
 
-
+def incremental_load_data(upload_df: pd.DataFrame, file_name: str):
+    stored_df = pd.read_parquet(file_name)
+    combined_df = pd.concat([upload_df, stored_df], ignore_index=True)
+    combined_df = combined_df.drop_duplicates()
+    combined_df.to_parquet(file_name)
+    return
